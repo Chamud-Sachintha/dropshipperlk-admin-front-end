408e17e4c9b8cb6f9d0638269333a4fd90d61006
Chamud-Sachintha <sheraanmario777@gmail.com>
2024-08-13 21:30:54 +0530

    test apis

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
-    apiURL: 'http://localhost:8000/api/',
-    fileServer: 'http://localhost:8001/kyc/',
-    fileServerBack: 'http://localhost:8001/',
+    // apiURL: 'http://localhost:8000/api/',
+    // fileServer: 'http://localhost:8001/kyc/',
+    // fileServerBack: 'http://localhost:8001/',
     isShowPopup: true,
-    // apiURL: 'https://testadminapi.dropshipper.lk/api/',
-    // fileServer: 'https://testadminapi.dropshipper.lk/kyc/',
-    // fileServerBack: 'https://testresellerapi.dropshipper.lk/',
-    // devServer: 'https://testadminapi.dropshipper.lk/'
+    apiURL: 'https://testadminapi.dropshipper.lk/api/',
+    fileServer: 'https://testadminapi.dropshipper.lk/kyc/',
+    fileServerBack: 'https://testresellerapi.dropshipper.lk/',
+    devServer: 'https://testadminapi.dropshipper.lk/'
 };
