877f4b633fc2b50002dbfdf5a7820e6ee596ddd3
Chamud-Sachintha <sheraanmario777@gmail.com>
2024-08-24 14:08:56 +0530

    live apis

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,11 +1,11 @@
 export const environment = {
-    apiURL: 'http://localhost:8000/api/',
-    fileServer: 'http://localhost:8001/kyc/',
-    fileServerBack: 'http://localhost:8001/',
-    devServer: 'http://localhost:8000/',
-    isShowPopup: true,
-    // apiURL: 'https://adminapi.dropshipper.lk/api/',
-    // fileServer: 'https://adminapi.dropshipper.lk/kyc/',
-    // fileServerBack: 'https://resellerapi.dropshipper.lk/',
-    // devServer: 'https://adminapi.dropshipper.lk/'
+    // apiURL: 'http://localhost:8000/api/',
+    // fileServer: 'http://localhost:8001/kyc/',
+    // fileServerBack: 'http://localhost:8001/',
+    // devServer: 'http://localhost:8000/',
+    isShowPopup: false,
+    apiURL: 'https://adminapi.dropshipper.lk/api/',
+    fileServer: 'https://adminapi.dropshipper.lk/kyc/',
+    fileServerBack: 'https://resellerapi.dropshipper.lk/',
+    devServer: 'https://adminapi.dropshipper.lk/'
  };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,11 @@
 export const environment = {
-    apiURL: 'http://localhost:8000/api/',
-    fileServer: 'http://localhost:8001/kyc/',
-    fileServerBack: 'http://localhost:8001/',
-    isShowPopup: true,
-    // apiURL: 'https://testadminapi.dropshipper.lk/api/',
-    // fileServer: 'https://testadminapi.dropshipper.lk/kyc/',
-    // fileServerBack: 'https://testresellerapi.dropshipper.lk/',
-    // devServer: 'https://testadminapi.dropshipper.lk/'
+    // apiURL: 'http://localhost:8000/api/',
+    // fileServer: 'http://localhost:8001/kyc/',
+    // fileServerBack: 'http://localhost:8001/',
+    // devServer: 'http://localhost:8000/',
+    isShowPopup: false,
+    apiURL: 'https://adminapi.dropshipper.lk/api/',
+    fileServer: 'https://adminapi.dropshipper.lk/kyc/',
+    fileServerBack: 'https://resellerapi.dropshipper.lk/',
+    devServer: 'https://adminapi.dropshipper.lk/'
 };
