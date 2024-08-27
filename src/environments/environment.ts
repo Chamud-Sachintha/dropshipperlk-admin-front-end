59a1bfea6473eee1979278b983487c8db5d509c5
Chamud-Sachintha <sheraanmario777@gmail.com>
2024-08-27 22:17:39 +0530

    live apis

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,8 @@ export const environment = {
     // fileServerBack: 'http://localhost:8001/',
     // devServer: 'http://localhost:8000/',
     isShowPopup: false,
-    apiURL: 'https://testadminapi.dropshipper.lk/api/',
-    fileServer: 'https://testadminapi.dropshipper.lk/kyc/',
-    fileServerBack: 'https://testresellerapi.dropshipper.lk/',
-    devServer: 'https://testadminapi.dropshipper.lk/'
+    apiURL: 'https://adminapi.dropshipper.lk/api/',
+    fileServer: 'https://adminapi.dropshipper.lk/kyc/',
+    fileServerBack: 'https://resellerapi.dropshipper.lk/',
+    devServer: 'https://adminapi.dropshipper.lk/'
 };
